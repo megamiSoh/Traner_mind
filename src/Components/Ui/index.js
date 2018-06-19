963722f21e4588af19e2fc4df70803fe08546d38
eunji soh <rekagirl@finalcompany.co.kr>
2018-06-19 11:22:47 +0900

    컴포넌트 UI단 내보내기

---
+++ src/Components/Ui/index.js
@@ -0,0 +1,54 @@
+// import ModalConfirm from "./Modal";
+import SearchBox from "./SearchBox";
+import { CardRevenue, CancelCardRevenue } from "./CardRevenue";
+import { GymRevenue, CancelGymRevenue } from "./GymRevenue";
+import Remark from "./Remark";
+import { ManageList, TotalManageList } from "./ManageList";
+import BCalendar from "./BCalendar";
+import {
+  MarcketCreateInfoFirst,
+  MarcketCreateInfoSecond
+} from "./MarcketCreateInfo";
+import Btn from "./Btn";
+import {
+  MarcketCreateTab,
+  OriginChildMenu,
+  OriginChildComponent
+} from "./MarcketCreateTab";
+import ManageSearchBox from "./ManageSearchBox";
+import AdInfo from "./AdInfo";
+import CaseSearch from "./CaseSearch";
+import Table from "./Table";
+import RegistMember from "./RegistMember";
+import CalendarSearch from "./CalendarSearch";
+import MobileTable from "./MobileTable";
+import CaseDetail from "./CaseDetail";
+import { BackBtn, RegstBtn, SearchBtn } from "./Btn";
+export {
+  BackBtn,
+  RegstBtn,
+  SearchBtn,
+  CaseDetail,
+  Btn,
+  MobileTable,
+  CalendarSearch,
+  RegistMember,
+  Table,
+  CaseSearch,
+  AdInfo,
+  SearchBox,
+  MarcketCreateTab,
+  CardRevenue,
+  CancelCardRevenue,
+  GymRevenue,
+  CancelGymRevenue,
+  MarcketCreateInfoFirst,
+  MarcketCreateInfoSecond,
+  OriginChildMenu,
+  OriginChildComponent,
+  Remark,
+  ManageSearchBox,
+  ManageList,
+  TotalManageList,
+  BCalendar
+};
