2168923f833c21e4c3e57de8a92489d32c7d616c
eunji soh <rekagirl@finalcompany.co.kr>
2018-06-19 11:15:20 +0900

    버튼 모음 컴포넌트

---
+++ src/Components/Ui/Btn.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+const BackBtn = ({ btn, location }) => (
+  <NavLink to={location} className="backBtn btnSpace">
+    {btn}
+  </NavLink>
+);
+const RegstBtn = ({ btn, onClick }) => (
+  <button className="registBtn btnSpace" onClick={onClick}>
+    {btn}
+  </button>
+);
+const SearchBtn = () => <button className="SearchBtn">검색</button>;
+export { BackBtn, RegstBtn, SearchBtn };
