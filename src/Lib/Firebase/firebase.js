6123ca30893649853eb40714f76598891db49d51
eunji soh <rekagirl@finalcompany.co.kr>
2018-06-19 11:32:02 +0900

    파이어베이스 sdk구성 초기화 정보

---
+++ src/Lib/Firebase/firebase.js
@@ -0,0 +1,32 @@
+import firebase from "firebase/app";
+import "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/database";
+
+const prodConfig = {
+  apiKey: "AIzaSyDIzo1bny_HFmwvxOQYx_4BFgvSPJWCuGQ",
+  authDomain: "auth-4d426.firebaseapp.com",
+  databaseURL: "https://auth-4d426.firebaseio.com",
+  projectId: "auth-4d426",
+  storageBucket: "auth-4d426.appspot.com",
+  messagingSenderId: "575953102109"
+};
+
+const devConfig = {
+  apiKey: "AIzaSyDIzo1bny_HFmwvxOQYx_4BFgvSPJWCuGQ",
+  authDomain: "auth-4d426.firebaseapp.com",
+  databaseURL: "https://auth-4d426.firebaseio.com",
+  projectId: "auth-4d426",
+  storageBucket: "auth-4d426.appspot.com",
+  messagingSenderId: "575953102109"
+};
+const config = process.env.NODE_ENV === "production" ? prodConfig : devConfig;
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+const database = firebase.database();
+const auth = firebase.auth();
+
+export { auth, database };
