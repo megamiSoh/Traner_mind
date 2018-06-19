814ccc2c0c0d67709be7e73f6c5ce771821ce917
eunji soh <rekagirl@finalcompany.co.kr>
2018-06-19 11:21:15 +0900

    텍스트 에어리아 컴포넌트(특이사항)

---
+++ src/Components/Ui/Remark.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Form, FormGroup, Label, Input } from "reactstrap";
+const Remark = () => (
+  <Form>
+    <FormGroup>
+      <Label>특이사항</Label>
+      <Input type="textarea" />
+    </FormGroup>
+  </Form>
+);
+export default Remark;
