ab4969a5320d99b5a78e1cd3b9aa35a56b564ea5
eunji soh <rekagirl@finalcompany.co.kr>
2018-06-19 11:30:20 +0900

    컨테이너 내보내기

---
+++ src/Container/Page/index.js
@@ -0,0 +1,18 @@
+import InfoWrap from "./InfoWrap";
+import Main from "./Main";
+import MarcketManageWrap from "./MarcketManageWrap";
+import MarcketWrap from "./MarcketWrap";
+import MarcketInfoDetail from "./MarcketInfoDetail";
+import RevenueResult from "./RevenueResult";
+import RevenueCalendar from "./RevenueCalendar";
+import CaseManage from "./CaseManage";
+export {
+  MarcketInfoDetail,
+  InfoWrap,
+  Main,
+  MarcketManageWrap,
+  MarcketWrap,
+  RevenueResult,
+  RevenueCalendar,
+  CaseManage
+};
