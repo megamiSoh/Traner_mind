ff3b777708e27a88ab12be959711060b4aace36f
eunji soh <rekagirl@finalcompany.co.kr>
2018-06-19 11:33:13 +0900

    통신 GET 리덕스 (DUCKS)

---
+++ src/Modules/getList.js
@@ -0,0 +1,57 @@
+import { handleActions } from "redux-actions";
+import { Map } from "immutable";
+import { database } from "../Lib/Firebase";
+// const GETLIST = "firebase";
+const GETLIST = "GETLIST";
+const GETFAILURE = "GETFAILURE";
+const GETPENDING = "GETPENDING";
+// export const getList = createAction(GETLIST);
+export const getList = id => dispatch => {
+  dispatch({ type: GETPENDING });
+  return database.cardInfo(id).on("value", function(snapshot) {
+    console.log(snapshot.exists());
+    if (snapshot.exists()) {
+      dispatch({
+        type: GETLIST,
+        payload: Object.values(snapshot.val())
+      });
+    } else {
+      dispatch({
+        type: GETFAILURE,
+        payload: Object.values(snapshot.val())
+      });
+    }
+  });
+};
+const initialState = Map({
+  getList: "",
+  error: false,
+  pending: false
+});
+
+export default handleActions(
+  {
+    [GETPENDING]: (state, action) => {
+      return {
+        ...state,
+        pending: true,
+        error: false
+      };
+    },
+    [GETLIST]: (state, action) => {
+      return {
+        ...state,
+        pending: false,
+        getList: action.payload
+      };
+    },
+    [GETFAILURE]: (state, action) => {
+      return {
+        ...state,
+        pending: false,
+        error: true
+      };
+    }
+  },
+  initialState
+);
