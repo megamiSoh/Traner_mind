07c78c92628d2b084cf94b3316e5e38c0d6cdc33
eunji soh <rekagirl@finalcompany.co.kr>
2018-06-21 10:50:23 +0900

    컴바인 리듀서에 post 추가

--- src/Modules/index.js
+++ src/Modules/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import authUser from "./authUser";
 import getList from "./getList";
+import postList from "./postList";
 export default combineReducers({
   auth: authUser,
-  getList
+  getList,
+  postList
 });
