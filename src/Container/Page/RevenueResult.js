cbedef94d2f2815d24381a8021849b51d03e8e02
eunji soh <rekagirl@finalcompany.co.kr>
2018-06-21 10:49:13 +0900

    기존 임의로 넣어놓은 state값 삭제후 파이어베이스 통신 함수 추가 / props추가

--- src/Container/Page/RevenueResult.js
+++ src/Container/Page/RevenueResult.js
@@ -4,61 +4,57 @@ import * as routes from "../../Constants/routes";
 import { MainWrap } from "../../Components/PageLayout";
 import { SearchBox } from "../../Components/Ui";
 import { Container } from "reactstrap";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import * as GetListActions from "../../Modules/getList";
 class RevenueResult extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      cardInfo: null,
       selectedDay: null,
-      revenueGymList: [
-        {
-          gymList: [
-            { name: "회원권", revenue: 100000000 },
-            { name: "PT", revenue: 10000000 },
-            { name: "필라테스", revenue: 1000000 },
-            { name: "골프", revenue: 100000 },
-            { name: "운동복", revenue: 10000 },
-            { name: "락커", revenue: 1000 },
-            { name: "위약금", revenue: 100 },
-            { name: "기타", revenue: 10 }
-          ],
-          gymCancelList: [
-            { name: "회원권", revenue: 200000000 },
-            { name: "PT", revenue: 20000000 },
-            { name: "필라테스", revenue: 2000000 },
-            { name: "골프", revenue: 200000 },
-            { name: "운동복", revenue: 20000 },
-            { name: "락커", revenue: 2000 },
-            { name: "위약금", revenue: 200 },
-            { name: "기타", revenue: 20 }
-          ]
-        }
-      ],
-      revenueList: [
-        {
-          cardList: [
-            { name: "롯데카드", revenue: 100000000 },
-            { name: "신한카드", revenue: 10000000 },
-            { name: "하나카드", revenue: 1000000 },
-            { name: "삼성카드", revenue: 100000 },
-            { name: "외환카드", revenue: 10000 },
-            { name: "비씨카드", revenue: 1000 },
-            { name: "농협카드", revenue: 100 },
-            { name: "기업카드", revenue: 10 }
-          ],
-          cardCancelList: [
-            { name: "롯데카드", revenue: 200000000 },
-            { name: "신한카드", revenue: 20000000 },
-            { name: "하나카드", revenue: 2000000 },
-            { name: "삼성카드", revenue: 200000 },
-            { name: "외환카드", revenue: 20000 },
-            { name: "비씨카드", revenue: 2000 },
-            { name: "농협카드", revenue: 200 },
-            { name: "기업카드", revenue: 20 }
-          ]
-        }
-      ]
+      Revenue: {},
+      gymList: [],
+      gymCancelList: [],
+      cardList: [],
+      cardCancelList: [],
+      totalSum: "",
+      totalRevenue: "",
+      totalCancel: ""
     };
   }
+  componentDidMount() {
+    this.getList("Revenue");
+    console.log(1);
+  }
+  componentWillReceiveProps(nextProps) {
+    if (
+      this.props.data === undefined ||
+      JSON.stringify(this.props.data) !== JSON.stringify(nextProps.data)
+    ) {
+      this.getList("Revenue");
+      console.log(2);
+    }
+  }
+  getList = async id => {
+    const { GetListActions } = this.props;
+    try {
+      await GetListActions.getList(id);
+      console.log("complete");
+      this.setState({
+        Revenue: this.props.data,
+        cardList: this.props.data.CardRevenue.Card,
+        cardCancelList: this.props.data.CardRevenue.CardCancel,
+        gymList: this.props.data.GymRevenue.Gym,
+        gymCancelList: this.props.data.GymRevenue.GymCancel,
+        totalSum: this.props.data.TotalRevenue,
+        totalRevenue: this.props.data.PureRevenue,
+        totalCancel: this.props.data.PureCancel
+      });
+    } catch (e) {
+      console.log(this.props.error);
+    }
+  };
   handleSelect(day) {
     // this.setState({ selectedDay: day });
     console.log(day);
@@ -76,8 +72,13 @@ class RevenueResult extends Component {
         />
         <div className="revenuWrap">
           <MainWrap
-            cardRevenue={this.state.revenueList}
-            gymRevenue={this.state.revenueGymList}
+            cardRevenue={this.state.cardList}
+            cancleCard={this.state.cardCancelList}
+            gymRevenue={this.state.gymList}
+            gymCancle={this.state.gymCancelList}
+            totalSum={this.state.totalSum}
+            totalRevenue={this.state.totalRevenue}
+            totalCancel={this.state.totalCancel}
           />
         </div>
         <div className="btnWrap">
@@ -89,4 +90,15 @@ class RevenueResult extends Component {
     );
   }
 }
-export default RevenueResult;
+export default connect(
+  state => ({
+    data: state.getList.getList,
+    error: state.getList.error,
+    pending: state.getList.pending
+    // data: getList.get("getList")
+  }),
+  dispatch => ({
+    GetListActions: bindActionCreators(GetListActions, dispatch)
+  })
+)(RevenueResult);
+// export default RevenueResult;
